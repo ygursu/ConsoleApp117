47c6c0f014af6046745383e6e3bc72babb1e1f46
ygursu <yasemingrs@gmail.com>
2024-03-05 00:20:25 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp117/Program.cs
@@ -0,0 +1,12 @@
+﻿double cifttoplam = 0, tektoplam = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("{0}. sayıyı girin:", i + 1);
+    double sayi = Convert.ToDouble(Console.ReadLine());
+    if (sayi % 2 == 0)
+        cifttoplam = cifttoplam + sayi;
+    else
+        tektoplam = tektoplam + sayi;
+}
+Console.WriteLine("Çiftlerin toplamı={0}\nTeklerin toplamı={1}", cifttoplam, tektoplam);
+Console.ReadKey();
